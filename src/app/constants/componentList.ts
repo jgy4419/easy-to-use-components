2809d4a5f6c97ec8dfe869e6bf049014af03ee38
정규영 <jgy_98@naver.com>
2024-07-21 15:46:36 +0900

    feat : 각 카테고리별로 값 가져오는 파일 생성

---
+++ src/app/constants/componentList.ts
@@ -0,0 +1,10 @@
+import Card1 from "../containeres/image/card/card1.png";
+import Card2 from "../containeres/image/card/card2.png";
+import Card3 from "../containeres/image/card/card3.png";
+
+export const itemList = {
+    Card: {
+        imgList: [Card1, Card2, Card3, Card1],
+        url: ["card1", "card2", "card3", "card1"]
+    },
+};
