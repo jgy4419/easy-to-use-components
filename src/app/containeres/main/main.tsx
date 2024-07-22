0551ba2c4c9d67dd357d9b085eff3ac365d1bed0
정규영 <jgy_98@naver.com>
2024-07-22 20:06:53 +0900

    feat : 메인 페이지 구현 (진행 중)

--- src/app/containeres/main/main.tsx
+++ src/app/containeres/main/main.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import * as S from "./style/main";
-import Cards from '../card/cards';
+import Image from "next/image";
+import { mainContent } from '@/app/constants/main';
+import profile from "@/app/assets/images/profile.jpeg";
+import { useRouter } from 'next/navigation';
 
 const Main = () => {
+    const route = useRouter();
     return (
         <>
             <S.Container>
                 <S.Inner>
-                    
+                    <S.Content>
+                        <S.Title>{ mainContent.title }</S.Title>
+                        <S.SubTitle> { mainContent.subTitle } </S.SubTitle>
+                        <S.Description> { mainContent.description } </S.Description>
+                        <S.Button onClick={() => route.push("/Card")}>Get started!</S.Button>
+                    </S.Content>
+                    <Image className='logoImg' src={profile} alt="프로필 이미지" width={200} height={200}/>
                 </S.Inner>
             </S.Container>
         </>
--- src/app/containeres/main/style/main.ts
+++ src/app/containeres/main/style/main.ts
@@ -1,9 +1,60 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+
+const contentLoad = keyframes`
+    0% {
+        transform: translateY(500%);
+    }
+    100% {
+        transform: translateY(0%);
+    }
+`;
 
 export const Container = styled.div`
     position: relative;
     width: 100vw;
     height: 100%;
+`;
+
+export const Content = styled.div`
+    line-height: 1;
+`;
+
+export const Title = styled.h1`
+    font-size: 70px;
+    animation: ${contentLoad} .5s;
+`;
+
+export const SubTitle = styled.h3`
+    font-size: 50px;
+    color: #e3e3e3;
+    animation: ${contentLoad} .7s;
+`;
+
+export const Description = styled.p`
+    font-size: 30px;
+    color: grey;
+    width: 70%;
+    animation: ${contentLoad} 1s;
+`;
+
+export const Button = styled.button`
+    border-radius: 10px;
+    width: 200px;
+    height: 80px;
+    font-size: 20px;
+    border: 5px solid #0F1214;
+    border-radius: 20px;
+    background-color: rgb(15,18,20);
+    color: #fff;
+    cursor: pointer;
+    transition: .2s;
+    animation: ${contentLoad} 1s;
+    &:hover {
+        border: 5px solid #1b6ca2;
+    }
+    &:active {
+        background-color: #1b6ca2;
+    }
 `;
 
 export const Inner = styled.div`
@@ -12,11 +63,22 @@ export const Inner = styled.div`
     inset: 0;
     margin: auto;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     align-items: center;
-`;
 
-export const Items = styled.ul`
-    width: 100%;
-    background-color: #eee;
+    @media (max-width: 1000px) {
+        flex-direction: column-reverse;
+        justify-content: center;
+        text-align: center;
+        ${Title} {
+            font-size: 50px;
+        }
+        ${SubTitle} {
+            font-size: 30px;
+        }
+        ${Description} {
+            width: 100%;
+            font-size: 20px;
+        }
+    }
 `;
