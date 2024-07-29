dd95a8ee724738b4647bf65c6933472733f445fa
정규영 <jgy_98@naver.com>
2024-07-29 21:53:21 +0900

    update : component 데이터 추가

--- src/app/constants/componentList.ts
+++ src/app/constants/componentList.ts
@@ -2,9 +2,54 @@ import Card1 from "../containeres/image/card/card1.png";
 import Card2 from "../containeres/image/card/card2.png";
 import Card3 from "../containeres/image/card/card3.png";
 
+// 나중에 DB에 추가
 export const itemList = {
     Card: {
-        imgList: [Card1, Card2, Card3, Card1],
-        url: ["card1", "card2", "card3", "card1"]
-    },
+        card1: {
+           url: "card1",
+           img: Card1,
+           className: {
+               title: "MainText",
+               description: "SubText",
+               backImage: "Card1Content",
+           },
+            type: {
+               title: "text",
+               description: "description",
+               backImage: "file"
+           }
+        },
+        card2: {
+            url: "card2",
+            img: Card2,
+            className: {
+                title: "MainTitle",
+                subTitle: "SubTitle",
+                description: "Description",
+                image: "ItemImage",
+            },
+            type: {
+                title: "text",
+                subTitle: "subTitle",
+                description: "description",
+                image: "img"
+            }
+        },
+        card3: {
+            url: "card2",
+            img: Card3,
+            className: {
+                title: "MainTitle",
+                subTitle: "SubTitle",
+                description: "SubText",
+                image: "ItemImage",
+            },
+            type: {
+                title: "text",
+                subTitle: "SubTitle",
+                description: "description",
+                image: "file"
+            }
+        },
+    }
 };
