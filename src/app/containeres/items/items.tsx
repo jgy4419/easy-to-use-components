bbf44695d0e07e955a9d57227ac508fe880826ea
정규영 <jgy_98@naver.com>
2024-07-29 21:58:06 +0900

    update : item list 불러오는 로직 수정

--- src/app/containeres/items/items.tsx
+++ src/app/containeres/items/items.tsx
@@ -4,19 +4,25 @@ import { usePathname } from "next/navigation";
 import ItemContainer from '@/app/components/itemContainer';
 import { itemList } from '@/app/constants/componentList';
 
-
 const Items = () => {
     const path = usePathname().split("/")[1];
     const items = itemList[path as keyof typeof itemList];
+    console.log(items);
     
     return (
         <S.Container>
             <S.Items>
-                {items.imgList.map((obj, index) => {
-                    return (
-                        <ItemContainer key={index} image={obj} path={items.url[index]} />
-                    );
-                })}
+                {
+                    Object.keys(items).map((key: string, index: number) => {
+                        /*
+                        * items 객체에서 특정 키에 해당하는 값을 가져오는 작업 수행.
+                        */
+                        const item = items[key as keyof typeof items];
+                        return (
+                            <ItemContainer key={index} obj={item} />
+                        )
+                    })
+                }
             </S.Items>
         </S.Container>
     );
