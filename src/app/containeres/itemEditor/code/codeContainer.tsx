e7bb3005c635c6f07db94c5788953473366fbabe
정규영 <jgy_98@naver.com>
2024-08-02 08:17:29 +0900

    feat : code Container 컴포넌트 구현, 컴포넌트 소스코드 넣는 기능 추가 (수정 및 분리 예정)

--- src/app/containeres/itemEditor/code/codeContainer.tsx
+++ src/app/containeres/itemEditor/code/codeContainer.tsx
@@ -1,15 +1,82 @@
 'use client';
-import React from 'react';
+import * as React from 'react';
 import * as S from "./style/codeContainer";
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+import { itemList } from "@/app/constants/componentList";
 
-const CodeContainer = () => {
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+        </div>
+    );
+}
+
+function a11yProps(index: number) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+export default function CodeContainer() {
+    const [value, setValue] = React.useState(0);
+    // TODO :  Card, card1 까지는 props나 redux 사용해서 가져오기
+    const { Card } = itemList;
+    const cardObjValue: {jsx: string, style: string} = Card.card1.code;
+
+    const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+        setValue(newValue);
+    };
+
     return (
         <S.Container>
             <S.Inner>
-
+                <Box sx={{ width: '100%', color: "#fff" }}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+                            {/*<Tab label="123" {...a11yProps(0)} sx={{color: "#fff", fontSize: "25px"}} />*/}
+                            {/*<Tab label="345" {...a11yProps(1)} sx={{color: "#fff", fontSize: "25px"}} />*/}
+                            {
+                                Object.keys(Card.card1.code).map((key, index) => {
+                                    return (
+                                        <Tab key={index} label={key} {...a11yProps(index)} sx={{color: "#fff", fontSize: "25px"}} />
+                                    )
+                                })
+                            }
+                        </Tabs>
+                    </Box>
+                    {
+                        Object.keys(Card.card1.code).map((key, index) => {
+                            const values: string = cardObjValue[key as keyof {jsx: string, style: string}];
+                            console.log('value', value);
+                            return (
+                                <CustomTabPanel value={value} index={index} key={index}>
+                                    <pre>
+                                        {values}
+                                    </pre>
+                                </CustomTabPanel>
+                            )
+                        })
+                    }
+                </Box>
             </S.Inner>
         </S.Container>
     );
-};
-
-export default CodeContainer;
+}
--- src/app/containeres/itemEditor/code/style/codeContainer.ts
+++ src/app/containeres/itemEditor/code/style/codeContainer.ts
@@ -1,8 +1,21 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+    position: relative;
+    width: 70%;
+    margin: 0 auto 20%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
 `;
 
-export const Inner = styled.div``
-;
+export const Inner = styled.div`
+    position: absolute;
+    //width: 70%;
+    //height: 80%;
+    inset: 0;
+    margin: auto;
+    border: 1px solid #fff;
+`;
 
