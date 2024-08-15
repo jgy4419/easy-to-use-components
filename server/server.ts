a56037d09d1d9d7ed45b3606827770e9c375c45c
정규영 <jgy_98@naver.com>
2024-08-15 16:34:57 +0900

    feat : express 서버 기본 세팅

---
+++ server/server.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response } from 'express';
+import next from 'next';
+import sequelize from './db';
+
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    const server = express();
+
+    // DB 연결 테스트
+    sequelize.authenticate()
+        .then(() => console.log('Database connected...'))
+        .catch(err => console.error('Error: ' + err));
+
+    // Custom API routes
+    // server.get('api/hello', (req: Request, res: Response) => {
+    //     res.json({ message: ' Hello from Express with TypeScript!!' })
+    // });
+    server.get('/api/users', async (req: Request, res: Response)=> {
+        try {
+            const [results, metadata] = await sequelize.query('SELECT * FROM Users');
+            res.json(results);
+        } catch(err) {
+            res.status(500).json({ error: "Database error "});
+        }
+    })
+
+    // Next.js 요청 처리
+    server.all('*', (req: Request, res: Response) => {
+        return handle(req, res);
+    });
+
+    server.listen(4000, (err?: any) => {
+        if(err) throw err;
+        console.log("> Ready on http://localhost:4000");
+    });
+})
