d4f997ff5ead26e9636ff8cde5da210550c785e9
정규영 <jgy_98@naver.com>
2024-08-31 14:08:02 +0900

    feat : 각 컴포넌트 참고사항 컴포넌트 추가

---
+++ src/app/containeres/itemEditor/code/style/note.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    //position: absolute;
+`;
+
+export const Inner = styled.div`
+`;
+
+export const Title = styled.h2`
+`;
+
+export const Description = styled.p`
+`;
