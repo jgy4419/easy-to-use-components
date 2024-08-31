cd2dd53ee5642c0115d75c8835062fa7707093e7
정규영 <jgy_98@naver.com>
2024-08-31 14:11:02 +0900

    chore : main section 구조 설정

--- src/app/containeres/main/main.tsx
+++ src/app/containeres/main/main.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
 import * as S from "./style/main";
-import Image from "next/image";
-import { mainContent } from '@/app/constants/main';
-import profile from "@/app/assets/images/profile.jpeg";
-import { useRouter } from 'next/navigation';
+import MainSection from "@/app/containeres/main/mainSection";
+import ProjectSection from "@/app/containeres/main/projectSection";
 
 const Main = () => {
-    const route = useRouter();
-    
     return (
         <>
             <S.Container>
-                <S.Inner>
-                    <S.Content>
-                        <S.Title>{ mainContent.title }</S.Title>
-                        <S.SubTitle> { mainContent.subTitle } </S.SubTitle>
-                        <S.Description> { mainContent.description } </S.Description>
-                        <S.Button onClick={() => route.push("/Card")}>Get started!</S.Button>
-                    </S.Content>
-                    <Image className='logoImg' src={profile} alt="프로필 이미지" width={200} height={200}/>
-                </S.Inner>
+                <MainSection />
             </S.Container>
+            <ProjectSection />
         </>
     );
 };
