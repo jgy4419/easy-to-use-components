dfd46690b7bfce91e73fd903115a45e8414d0332
정규영 <jgy_98@naver.com>
2024-08-31 14:04:48 +0900

    feat : 메인 페이지의 프로젝트 소개하는 section ui 추가

---
+++ src/app/containeres/main/projectSection.tsx
@@ -0,0 +1,53 @@
+'use client';
+import React, {useState} from 'react';
+import * as S from "./style/projectSection";
+import Image from "next/image";
+import {projectSection} from "@/app/constants/main";
+import {useRouter} from "next/navigation";
+
+// 컴포넌트 분리하기
+const ProjectSection = () => {
+    const {imgContent, content} = projectSection;
+    const [imageButtonIndex, setImageButtonIndex] = useState(1);
+    const route = useRouter();
+
+    return (
+        <S.Container>
+            <S.Inner>
+                <S.ImgContent>
+                    <S.ImgTitle>{imgContent.title}</S.ImgTitle>
+                    <S.ImgZip>
+                        {
+                            imgContent.img.map((img, index) => {
+                                return (
+                                    <S.Img key={index} index={imageButtonIndex}>
+                                        <Image src={img} alt="이미지" width={200}/>
+                                    </S.Img>
+                                )
+                            })
+                        }
+                    </S.ImgZip>
+                    <S.ImgButtons>
+                    {
+                        imgContent.img.map((_, index) => {
+                            return (
+                                <S.ImgButton
+                                    onClick={() => setImageButtonIndex(index + 1)}
+                                    index={imageButtonIndex} key={index}
+                                />
+                            )
+                        })
+                    }
+                    </S.ImgButtons>
+                </S.ImgContent>
+                <S.Content>
+                    <S.Title>{content.title}</S.Title>
+                    <S.Description>{content.description}</S.Description>
+                    <S.Button onClick={() => route.push("/Card")}>{content.button}</S.Button>
+                </S.Content>
+            </S.Inner>
+        </S.Container>
+    );
+};
+
+export default ProjectSection;
---
+++ src/app/containeres/main/style/projectSection.ts
@@ -0,0 +1,135 @@
+import styled from "styled-components";
+
+export const Inner = styled.div`
+    position: absolute;
+    width: 70%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    inset: 0;
+    margin: auto;
+`;
+
+export const ImgContent = styled.div`
+    width: 50%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+`;
+
+export const ImgTitle = styled.h1`
+    font-size: 40px;
+`;
+
+export const ImgZip = styled.div`
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 250px;
+`;
+
+export const Img = styled.div<{index: number}>`
+    position: absolute;
+    opacity: 0;
+    transition: .3s;
+    &:nth-child(${props => props.index}) {
+        opacity: 1;
+    }
+`;
+
+export const ImgButtons = styled.div`
+    position: relative;
+    display: flex;
+    gap: 10px;
+`;
+
+export const ImgButton = styled.button<{index: number}>`
+    width: 40px;
+    height: 40px;
+    border-radius: 50%;
+    background-color: #1b6ca2;
+    border: 4px solid #1b6ca2;
+    transition: .3s;
+    cursor: pointer;
+
+    &:nth-child(${props => props.index}) {
+        border: 4px solid #fff;
+    }
+`;
+
+export const Content = styled.div`
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Title = styled.h1`
+`;
+
+export const Description = styled.p`
+    width: 500px;
+    font-size: 18px;
+    line-height: 2;
+    color: grey;
+`;
+
+export const Button = styled.button`
+    width: 200px;
+    height: 60px;
+    font-size: 20px;
+    border: 3px solid #0F1214;
+    border-radius: 20px;
+    background-color: rgb(15, 18, 20);
+    color: #fff;
+    cursor: pointer;
+    transition: .2s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 5px;
+
+    &:hover {
+        border: 3px solid #1b6ca2;
+    }
+
+    &:active {
+        background-color: #1b6ca2;
+    }
+`;
+
+export const Container = styled.div`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+
+    @media (max-width: 1000px) {
+        ${Inner} {
+            flex-direction: column;
+        }
+        ${ImgContent} {
+            height: 40%;
+        }
+        ${Content} {
+            text-align: center;
+            margin-top: 50px;
+        }
+        ${Description} {
+            width: 100%;
+            font-size: 15px;
+        }
+        ${Button} {
+            margin: auto;
+        }
+        ${Img} {
+            top: 10px;
+        }
+        ${ImgButtons} {
+            top: 50px;
+        }
+    }
+`;
