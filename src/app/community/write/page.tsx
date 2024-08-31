b0fa28b9deb3d976e030dd7c41ee9051f8e8487d
정규영 <jgy_98@naver.com>
2024-08-31 13:53:09 +0900

    feat : 글 작성 페이지 틀 작업

---
+++ src/app/community/write/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import React from 'react';
+import styled from "styled-components";
+import Caution from "@/app/components/caution";
+import MessageForm from "@/app/containeres/write/messageForm";
+
+const Page = () => {
+    return (
+        <Container>
+            <Caution />
+            <MessageForm />
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    position: relative;
+    width: 100vw;
+`;
+
+export default Page;
