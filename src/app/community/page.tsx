a40112f7545fa8d5ff9326ed568614a4ddbdf68a
정규영 <jgy_98@naver.com>
2024-08-31 13:52:35 +0900

    feat : community 페이지 구조 설정

---
+++ src/app/community/page.tsx
@@ -0,0 +1,25 @@
+'use client';
+import React from 'react';
+import styled from "styled-components";
+import Search from "@/app/containeres/community/search";
+import List from "@/app/containeres/community/list";
+import Caution from "@/app/components/caution";
+
+
+const Page = () => {
+    return (
+        <Container>
+            <Caution />
+            <Search/>
+            <List />
+        </Container>
+    );
+};
+
+const Container = styled.main`
+    position: relative;
+    width: 100vw;
+    height: 100vh;
+`;
+
+export default Page;
