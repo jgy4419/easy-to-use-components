c5b18ffe483c47f3d58b756fe16bd9b910d2415b
정규영 <jgy_98@naver.com>
2024-09-01 14:27:43 +0900

    feat : 게시글 상세보기 페이지 추가

---
+++ src/app/containeres/community/postDetail/style/postDetail.ts
@@ -0,0 +1,67 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    position: relative;
+    width: 80%;
+    margin: auto;
+    
+`;
+
+export const Inner = styled.div`
+
+`;
+
+export const Content = styled.section`
+    position: relative;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    background: #0F1214;
+    padding: 20px;
+    border-radius: 20px;
+`;
+
+export const ContentHeader = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Line = styled.hr`
+    // background: grey;
+    width: calc(100% + 35px);
+    margin-left: -20px;
+    border: 1px solid grey;
+`;
+
+export const ContentTitle = styled.h2`
+
+`;
+
+export const ContentSubInfo = styled.div`
+    display: flex;
+    gap: 20px;
+    color: grey;
+    font-size: 14px;
+`;
+
+export const ContentName = styled.p`
+`;
+
+export const ContentDate = styled.p`
+`;
+
+export const ContentBody = styled.p`
+    min-height: 500px;
+`;
+
+export const MoveButtonContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    gap: 20px;
+`;
+
+export const ListButton = styled.button`
+`;
+
+export const CreateButton = styled.button`
+`;
