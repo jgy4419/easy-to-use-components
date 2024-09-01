7c30494e979ac44932dce7ac1510942f2bfb7416
정규영 <jgy_98@naver.com>
2024-09-01 14:29:45 +0900

    update : 게시판 리스트 클릭 시 상세 보기 페이지로 이동 시켜주기

--- src/app/containeres/community/listItem.tsx
+++ src/app/containeres/community/listItem.tsx
@@ -1,11 +1,13 @@
 'use client';
 import React, {useEffect, useState} from 'react';
+import { useRouter } from "next/navigation";
 import * as S from "@/app/containeres/community/style/list";
 
 const ListItem = ({data}: {idx: number, title: string, date: string}) => {
+    const router = useRouter();
     return (
         <>
-            <S.BodyList>
+            <S.BodyList onClick={() => router.push("community/" + data.idx)}>
                 <S.BodyItem>{data.idx}</S.BodyItem>
                 <S.BodyItem>{data.title}</S.BodyItem>
                 <S.BodyItem>{data.date}</S.BodyItem>
