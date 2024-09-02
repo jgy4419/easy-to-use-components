126cb6fcb7af9c63595367de0e7f482ab0b9b5d5
정규영 <jgy_98@naver.com>
2024-09-02 20:39:57 +0900

    feat : 게시글 리스트 불러오는 기능 구현

--- src/app/containeres/community/list.tsx
+++ src/app/containeres/community/list.tsx
@@ -1,63 +1,68 @@
 "use client";
 
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from "./style/list";
 import { tableHead } from "@/app/constants/caution";
 import ListItem from "@/app/containeres/community/listItem";
+import { ICommunity } from "./type/type";
 
 const List = () => {
-    const [testData, setTestData] = useState([
-        {
-            idx: 1,
-            title: "제목이에용",
-            date: "2024 / 07 / 31"
-        },
-        {
-            idx: 2,
-            title: "제목이에용2",
-            date: "2024 / 08 / 31"
-        },
-        {
-            idx: 3,
-            title: "제목이에용3",
-            date: "2024 / 05 / 31"
-        }
-    ]);
-
     const [isClient, setIsClient] = useState(false);
 
+    const [communityData, setCommunityData] = useState<ICommunity[]>([]);
+
     useEffect(() => {
-        testData !== null && setIsClient(true);
+        getCommunityData();
     }, []);
+
+    const getCommunityData = async () => {
+        try {
+            const response = await fetch("/api/community");
+
+            if(!response.ok) {
+                throw new Error("게시글들을 불러오는데 실패했습니다.");
+                
+            }
+            const data = await response.json();
+
+            console.log(data);
+            setCommunityData(data);
+
+        } catch(err) {
+            console.log("데이터를 불러오는데 실패했습니다." + err);
+        }
+    }
+
+    // useEffect(() => {
+        // testData !== null && setIsClient(true);
+    // }, []);
 
     return (
         <S.Container>
             {
-                isClient
-                    ? <S.Table>
-                        <S.Head>
-                            {
-                                tableHead.map((list, index) => {
-                                    return (
-                                        <S.HeadItem key={index}>{list}</S.HeadItem>
-                                    )
-                                })
-                            }
-                        </S.Head>
-                        <S.Body>
-                            {
-                                testData.map((data, index) => {
-                                    return (
-                                        <ListItem
-                                            key={data.idx} 
-                                            data={data}
-                                        />
-                                    )
-                                })
-                            }
-                        </S.Body>
-                    </S.Table>
-                    : <>문의 내역이 없습니다!</>
+                <S.Table>
+                    <S.Head>
+                        {
+                            tableHead.map((list, index) => {
+                                return (
+                                    <S.HeadItem key={index}>{list}</S.HeadItem>
+                                )
+                            })
+                        }
+                    </S.Head>
+                    <S.Body>
+                        {
+                            communityData.map((data: ICommunity, index: number) => {
+                                return (
+                                    <ListItem
+                                        key={index} 
+                                        data={data}
+                                    />
+                                )
+                            })
+                        }
+                    </S.Body>
+                </S.Table>
             }
 
         </S.Container>
