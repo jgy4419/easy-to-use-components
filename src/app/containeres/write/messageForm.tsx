ad52a38f51533a36d292a0d208be0fe69c1f91f0
정규영 <jgy_98@naver.com>
2024-09-16 17:18:45 +0900

    feat : 게시글 수정 시 기존에 작성된 text 불러오기 및 update 시켜주는 함수 분기처리

--- src/app/containeres/write/messageForm.tsx
+++ src/app/containeres/write/messageForm.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from "./style/messageForm";
 import List from "./list";
 import Box from "@mui/material/Box";
@@ -16,7 +16,8 @@ type ActionMap = {
     content: typeof contentChange
 }
 
-const MessageForm = () => {
+const MessageForm = ({ postIndex }: {postIndex: string | -1}) => {
+    // 수정하기
     const testComponentList = ["Card", "Button"];
     const testNameList = ["Card1", "Card2"];
 
@@ -24,6 +25,16 @@ const MessageForm = () => {
     const router = useRouter();
 
     const { title, name, content, password, category, component } = useSelector((state: RootState) => state.community);
+    const [formData, setFormData] = useState({
+        title: "",
+        content: "",
+        component: "",
+        date: "",
+        idx: -1,
+        name: "",
+        password: "",
+        category: ""
+    });
 
     const textChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>, item: keyof ActionMap) => {
         const value = event.target.value;
@@ -51,32 +62,64 @@ const MessageForm = () => {
             content: content,
             password: password,
             category: category,
-            component: component
+            component: component,
+            updateIndex: formData.idx
         };
 
         console.log("data 전송", data);
 
-        await fetch("/api/community/write", {
-            method: "PUT",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data)
-        }).then(() => {
-            router.back();
-        });
+        if(formData.idx === -1) {
+            await fetch("/api/community/write", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
+            }).then(() => {
+                router.back();
+            });
+        } else {
+            await fetch("/api/community/postEdit", {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(data)
+            }).then(() => {
+                router.back();
+            }); 
+        }
+    }
+
+    useEffect(() => {
+        console.log("updateIndex", postIndex);
+        if(postIndex !== -1) {
+            postData();
+        }
+    }, [postIndex]);
+
+    const postData = async () => {
+        const response = await fetch(`/api/community/detail?idx=${postIndex}`);
+
+        if(!response.ok) {
+            throw new Error("게시글을 불러오는데 실패했습니다.");
+        }
+        const data = await response.json();
+        
+        setFormData(data[0]);
+        console.log(data[0]);
     }
 
     return (
         <S.Container>
             <S.FormBox>
-                <S.TitleInput onChange={(event) => textChange(event, "title")} placeholder='제목을 입력해주세요.' type="text" />
+                <S.TitleInput onChange={(event) => textChange(event, "title")} placeholder={formData.title} type="text" />
                 <S.Information>
                     <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
                         <AccountCircle sx={{ color: '#fff', mr: 1, my: 0.5 }} />
                         <S.UserName 
                             id="input-with-lx" 
-                            label="name" 
+                            label={formData.name}
                             variant="standard" 
                             onChange={(event) => textChange(event, "name")}
                         />
@@ -86,12 +129,18 @@ const MessageForm = () => {
                 </S.Information>
                 <S.MessageTextArea
                     id="outlined-multiline-static"
-                    label="내용을 입력해주세요"
+                    label={formData.content}
                     multiline
                     rows={4}
                     onChange={(event) => textChange(event, "content")}
                 />
-                <S.SubmitButton onClick={submitHandler}>Submit</S.SubmitButton>
+                <S.SubmitButton onClick={submitHandler}>
+                    {
+                        formData.idx === -1
+                            ? "Submit"
+                            : "Update"
+                    }
+                </S.SubmitButton>
             </S.FormBox>
         </S.Container>
     );
