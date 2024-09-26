5e0ec227e4761e7a3cb87d201a422d188cfc3c1e
정규영 <jgy_98@naver.com>
2024-09-26 20:46:12 +0900

    update : 카테고리에 맞는 컴포넌트들 db에서 가져온 값으로 뿌려주도록 수정

--- src/app/containeres/items/items.tsx
+++ src/app/containeres/items/items.tsx
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import * as S from "./style/items";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import ItemContainer from '@/app/components/itemContainer';
-import { itemList } from '@/app/constants/componentList';
 import ErrorBoundary from "@/app/containeres/Error/ErrorBoundary";
 import NoItems from "@/app/containeres/Error/NoItems";
 import CategoryList from '@/app/components/categoryList';
+import { apiGet } from '@/app/util/apiModule';
 
 const Items = () => {
     const path = usePathname().split("/")[1];
-    const items = itemList[path as keyof typeof itemList];
+    const [items, setItems] = useState<any[]>([]);
+    
+    const itemListChange = async (path: string) => {
+        const response = await apiGet(`components/componentList?name="${path}"`, "컴포넌트들을 불러오지 못했습니다.");
+
+        setItems(prev => [...prev, ...response]);
+        
+    }
+
+    useEffect(() => {
+        itemListChange(path);
+    }, [path]);
 
     if (!items) {
         // items가 없으면 에러 처리 또는 대체 컴포넌트 렌더링
@@ -22,11 +33,11 @@ const Items = () => {
                 <CategoryList />
                 <S.Items>
                     {
-                        Object.keys(items).map((key: string, index: number) => {
+                        items.map((item: string, index: number) => {
                             /*
                             * items 객체에서 특정 키에 해당하는 값을 가져오는 작업 수행.
                             */
-                            const item = items[key as keyof typeof items];
+                            // const item = items[key as keyof typeof items];
                            
                             return (
                                 <ItemContainer key={index} obj={item} />
