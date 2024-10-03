8eeeefef312e6cacaba92619bb31791ce6a51626
정규영 <jgy_98@naver.com>
2024-10-03 16:57:47 +0900

    update : 각 카테고리에 컴포넌트 개수가 없을 때 컴포넌트 없을 때 생성시켜주는 UI 적용

--- src/app/containeres/items/items.tsx
+++ src/app/containeres/items/items.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import * as S from "./style/items";
-import { usePathname, useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import ItemContainer from '@/app/components/itemContainer';
 import ErrorBoundary from "@/app/containeres/Error/ErrorBoundary";
 import NoItems from "@/app/containeres/Error/NoItems";
@@ -22,7 +22,7 @@ const Items = () => {
         itemListChange(path);
     }, [path]);
 
-    if (!items) {
+    if (items.length === 0) {
         // items가 없으면 에러 처리 또는 대체 컴포넌트 렌더링
         return <NoItems item={path}/>
     }
@@ -33,7 +33,7 @@ const Items = () => {
                 <CategoryList />
                 <S.Items>
                     {
-                        items.map((item: string, index: number) => {
+                        items.map((item, index: number) => {
                             /*
                             * items 객체에서 특정 키에 해당하는 값을 가져오는 작업 수행.
                             */
