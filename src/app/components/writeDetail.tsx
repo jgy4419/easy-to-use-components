5ce08bf462b11cc1e625085634019defd3f97011
정규영 <jgy_98@naver.com>
2024-10-20 07:40:54 +0900

    feat : 게시글 상세 페이지 컴포넌트

---
+++ src/app/components/writeDetail.tsx
@@ -0,0 +1,20 @@
+"use client";
+import styled from "styled-components";
+import Caution from "@/app/components/caution";
+import MessageForm from "@/app/containeres/write/messageForm";
+
+const WriteDetail = () => {
+    return (
+        <Container>
+            <Caution />
+            <MessageForm/>
+        </Container>
+    );
+};
+
+const Container = styled.div`
+    position: relative;
+    width: 100vw;
+`;
+
+export default WriteDetail;
